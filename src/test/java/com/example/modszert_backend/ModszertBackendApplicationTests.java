86e1fdd1fdd0f97ef31e718fcf345c4d33c89c90
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-11-06 20:52:45 +0100

    initialize

---
+++ src/test/java/com/example/modszert_backend/ModszertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modszert_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModszertBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
