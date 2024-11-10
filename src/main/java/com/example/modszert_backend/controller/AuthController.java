62d6f5057d9cc84f62d4b6b61a90d3de80798f04
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-11-10 13:39:54 +0100

    Add JWT authentication and setup Postgres DB

---
+++ src/main/java/com/example/modszert_backend/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.modszert_backend.controller;
+
+import com.example.modszert_backend.dto.RegisterDto;
+import com.example.modszert_backend.dto.LoginDto;
+import com.example.modszert_backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterDto registerDto) {
+        return ResponseEntity.ok(authService.register(registerDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(authService.login(loginDto));
+    }
+}
